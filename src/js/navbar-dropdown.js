13394a5584f9d3ea540b3e699d5bd89d13bcf964
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-01-28 13:42:03 +0800

    separating script to new file

--- src/js/navbar-dropdown.js
+++ src/js/navbar-dropdown.js
@@ -4,3 +4,7 @@ const dropdownMenu = document.getElementById('dropdown-menu');
 toggleButton.addEventListener('click', () => {
     dropdownMenu.classList.toggle('hidden');
 });
+
+function closeDropdown() {
+    dropdownMenu.classList.add('hidden');
+}
