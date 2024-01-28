bf61c4a09b732e09bc8a3fc49fcc05259921bca9
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-01-28 15:05:42 +0800

    fix responsive style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xs': '0px',
+      },
       fontFamily: {
         roboto: ['Roboto']
       }
