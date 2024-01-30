c1e363b9701681e912740e2da999bc402b4f3730
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-01-30 13:42:13 +0800

    make button change when dropdown

--- src/js/navbar-dropdown.js
+++ src/js/navbar-dropdown.js
@@ -1,4 +1,19 @@
-function Menu(e) {
+function Menu() {
     let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
+    let menuIcon = document.getElementById('menuIcon');
+    let closeIcon = document.getElementById('closeIcon');
+
+    if (menuIcon.style.display !== 'none') {
+        menuIcon.style.display = 'none';
+        closeIcon.style.display = 'inline-block';
+
+        list.classList.add('top-[80px]');
+        list.classList.add('opacity-100');
+    } else {
+        menuIcon.style.display = 'inline-block';
+        closeIcon.style.display = 'none';
+
+        list.classList.remove('top-[80px]');
+        list.classList.remove('opacity-100');
+    }
 }
